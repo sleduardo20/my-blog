acd204ac124906e5c226da60567119d2557153c8
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-12 12:07:51 -0300

    ⚙ chore: config images, deice sizes, domains

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    dest: "public",
     disable: !isProd,
+  },
+  images: {
+    deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 1400],
+    domains: ["images.unsplash.com"],
   },
 });
