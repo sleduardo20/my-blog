7c480433039b201de647ab57fb363d48e4ec7323
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-12 14:12:41 -0300

    ✏ feat: make break ponints in Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,15 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div`
-  width: min(100%, 110rem);
+  width: min(100%, 117rem);
   margin: 0 auto;
+
+  ${media.lessThan("medium")`
+    width: min(100%, 76.8rem);
+  `}
+
+  ${media.lessThan("small")`
+    width: min(100%, 45rem);
+  `}
 `;
