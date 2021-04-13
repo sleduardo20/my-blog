8f3f0d591381a922b37b6b889cf57ebc284d6576
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-13 09:29:33 -0300

    ⛏  feat: add properties to Banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 
 import * as S from "./styles";
 
-export const Banner = () => {
+export interface BannerProps {
+  src: string;
+}
+
+export const Banner = ({ src }: BannerProps) => {
   return (
     <S.Wrapper>
-      <Image src="/img/banner.jpg" layout="fill" objectFit="cover" />
+      <Image src={src} layout="fill" objectFit="cover" />
     </S.Wrapper>
   );
 };
--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -1,10 +1,14 @@
 import { Meta, Story } from "@storybook/react";
 
-import { Banner } from ".";
+import { Banner, BannerProps } from ".";
 
 export default {
   title: "components/Banner",
   component: Banner,
+  args: {
+    src:
+      "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80",
+  },
 } as Meta;
 
-export const Basic: Story = (args) => <Banner {...args} />;
+export const Basic: Story<BannerProps> = (args) => <Banner {...args} />;
