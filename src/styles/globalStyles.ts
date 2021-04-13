33b1e6a4c5fc878fc5da53ba7616014e5c69fecd
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-13 09:34:09 -0300

    🎨 styles: add font face nunito 300

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
+@font-face {
+  font-family: 'Nunito';
+  font-style: normal;
+  font-weight: 300;
+  src: local(''),
+       url('../fonts/nunito-v16-latin-300.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('../fonts/nunito-v16-latin-300.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+
   @font-face {
   font-family: 'Nunito';
   font-style: normal;
@@ -46,7 +55,7 @@ export default createGlobalStyle`
   font-family: ${({ theme }) => theme.font.family};
   font-size: ${({ theme }) => theme.font.size.md};
   color: ${({ theme }) => theme.colors.gray800};
-  line-height: 22px;
+  line-height: ${({ theme }) => theme.font.lineHeight.xsm};
   }
 
   body {
