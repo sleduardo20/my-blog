237f6d381cb365c085b329012c4bc8d6d5442f8d
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-13 13:23:42 -0300

    🚀 feat: make implementation the api prismic in Home page and get data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,32 @@
+import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
+
 import { Banner } from "components/Banner";
-import { CardPost } from "components/CardPost";
+import { CardPost, CardPostProps } from "components/CardPost";
 import { Container } from "components/Container";
 import { Header } from "components/Header";
 
+import { getClientPrimisc } from "../services/prismic";
+
 import * as S from "./styles";
 
-import mockCard from "../components/CardPost/mock";
+interface HomeProps {
+  srcBanner: string;
+  cards: CardPostProps[];
+}
 
-const Home = () => {
+const Home = ({ srcBanner, cards }: HomeProps) => {
   return (
     <S.Wrapper>
       <Header />
-      <Banner />
+      <Banner src={srcBanner} />
       <Container>
         <S.Main>
           <h1>Posts Destaques</h1>
           <S.Cards>
-            <CardPost {...mockCard} />
-            <CardPost {...mockCard} />
-            <CardPost {...mockCard} />
-            <CardPost {...mockCard} />
-            <CardPost {...mockCard} />
+            {cards.map((card) => (
+              <CardPost key={card.title} {...card} />
+            ))}
           </S.Cards>
         </S.Main>
       </Container>
@@ -29,3 +35,40 @@ const Home = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const prismic = getClientPrimisc();
+
+  const bannerResponse = await prismic.query([
+    Prismic.predicates.at("document.type", "banner"),
+  ]);
+
+  const cardsResponse = await prismic.query([
+    Prismic.predicates.at("document.type", "post"),
+  ]);
+
+  const banner = String(
+    bannerResponse.results.map((result) => result.data.src)
+  );
+
+  if (!cardsResponse) {
+    throw new Error("Error request prismic");
+  }
+
+  const cards = cardsResponse.results.map(
+    (card): CardPostProps => ({
+      slug: card.uid,
+      title: card.data.title,
+      subtitle: card.data.subtitle,
+      img: card.data.imagepost,
+      publisher: card.first_publication_date || "",
+    })
+  );
+
+  return {
+    props: {
+      srcBanner: banner,
+      cards,
+    },
+  };
+};
