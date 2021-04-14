c0cad3d2bb90ed19ceb77c73f7d4dfe0afa2e940
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-14 17:40:15 -0300

    ⛏ feat: alter structure and add styles

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import { RichText } from "prismic-dom";
+
 import * as S from "./styles";
 
+type Body = {
+  text: string;
+};
+
 export interface ContentProps {
-  titlecontent?: string;
-  p1: string;
-  p2?: string;
-  p3?: string;
-  code?: string;
-  imagecontent?: {
+  heading?: string;
+  body: Body[];
+  imagecontent: {
     url: string;
     alt: string;
     dimensions: {
@@ -17,41 +20,18 @@ export interface ContentProps {
   };
 }
 
-export const Content = ({
-  titlecontent,
-  p1,
-  p2,
-  p3,
-  code,
-  imagecontent,
-}: ContentProps) => {
+export const Content = ({ heading, body, imagecontent }: ContentProps) => {
   return (
     <S.Container>
-      {!!titlecontent && <h2>{titlecontent}</h2>}
-
-      <S.Text>{p1}</S.Text>
-
-      {!!p2 && <S.Text>{p2}</S.Text>}
-      {!!p3 && <S.Text>{p3}</S.Text>}
-
-      {!!code && (
-        <S.Code>
-          <pre>
-            <code>{code}</code>
-          </pre>
-        </S.Code>
-      )}
-
-      {!!imagecontent && (
-        <S.Figure>
-          <Image
-            className="post-image"
-            src={imagecontent.url}
-            width={imagecontent.dimensions.width}
-            height={imagecontent.dimensions.height}
-          />
-        </S.Figure>
-      )}
+      {!!heading && <h2>{heading}</h2>}
+      <S.Body dangerouslySetInnerHTML={{ __html: RichText.asHtml(body) }} />
+      <S.Figure>
+        <img
+          className="post-image"
+          src={imagecontent.url}
+          alt={imagecontent.alt}
+        />
+      </S.Figure>
     </S.Container>
   );
 };
--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -3,8 +3,9 @@ import styled, { css } from "styled-components";
 export const Container = styled.aside`
   ${({ theme }) => css`
     max-width: 70rem;
-    padding: ${theme.spacings.sm};
-    margin: 5.4rem auto 0;
+    height: max-content;
+    padding: 0 ${theme.spacings.xs};
+    margin: ${theme.spacings.xss} auto 0;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -18,34 +19,32 @@ export const Container = styled.aside`
   `}
 `;
 
-export const Text = styled.p`
+export const Body = styled.div`
   ${({ theme }) => css`
-    font-size: ${theme.font.size.md};
-    font-weight: ${theme.font.weightLight};
-    line-height: ${theme.font.lineHeight.sm};
-    text-align: justify;
-    margin: ${theme.spacings.xs} 0;
-  `}
-`;
-
-export const Code = styled.pre`
-  ${({ theme }) => css`
-    width: 90%;
-    background: ${theme.colors.gray800};
-    display: flex;
-    justify-content: center;
-    margin: ${theme.spacings.xlg} 0;
-    border-radius: ${theme.spacings.xs};
-
-    pre {
-      max-width: 100%;
-      padding: ${theme.spacings.xs};
-      overflow-x: auto;
+    p,
+    li {
+      text-align: justify;
+      font-weight: ${theme.font.weightLight};
+      line-height: ${theme.font.lineHeight.sm};
+      margin-bottom: ${theme.spacings.sm};
     }
 
-    code {
-      color: ${theme.colors.yellow500};
-      padding: 0;
+    ul,
+    ol {
+      padding: ${theme.spacings.md} ${theme.spacings.xlg};
+      list-style: disc;
+      list-style-image: none;
+    }
+
+    pre {
+      margin: ${theme.spacings.xs} auto;
+      background: ${theme.colors.gray100};
+      border-radius: 4px;
+      padding: ${theme.spacings.sm};
+    }
+
+    a {
+      color: ${theme.colors.lightBlue};
     }
   `}
 `;
@@ -54,7 +53,6 @@ export const Figure = styled.div`
   ${({ theme }) => css`
     display: block;
     margin: ${theme.spacings.xlg} 0;
-
     .post-image {
       border-radius: 8px;
     }
