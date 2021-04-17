15a640c0ea4d338d7cddbba0dd741fba787c94e6
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-17 13:03:59 -0300

    🔧 fix: alter implementaion, add condition

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,20 +1,20 @@
-import { Component } from "react";
-
-export class Comments extends Component {
-  componentDidMount() {
-    const script = document.createElement("script");
-    const anchor = document.getElementById("inject-comments-for-uterances");
-    script.setAttribute("src", "https://utteranc.es/client.js");
-    script.setAttribute("crossorigin", "anonymous");
-    script.setAttribute("label", "Deixe seu comentário 👍");
-    script.setAttribute("async", true);
-    script.setAttribute("repo", "sleduardo20/my-blog");
-    script.setAttribute("issue-term", "pathname");
-    script.setAttribute("theme", "github-light");
-    anchor.appendChild(script);
-  }
-
-  render() {
-    return <div id="inject-comments-for-uterances" />;
-  }
-}
+export const Comments: React.FC = () => {
+  return (
+    <section
+      ref={(elem) => {
+        if (!elem || elem.childNodes.length) {
+          return;
+        }
+        const script = document.createElement("script");
+        script.setAttribute("src", "https://utteranc.es/client.js");
+        script.setAttribute("crossorigin", "anonymous");
+        script.setAttribute("label", "Deixe seu comentário 👍");
+        script.async = true;
+        script.setAttribute("repo", "sleduardo20/my-blog");
+        script.setAttribute("issue-term", "pathname");
+        script.setAttribute("theme", "github-light");
+        elem.appendChild(script);
+      }}
+    />
+  );
+};
