df7547bd3574770273dfe63898d52324b954bad6
sleduardo20 <sl.eduardo20@gmail.com>
2021-04-16 22:44:50 -0300

    🧯 fix: add mock data correctly

--- src/components/Content/stories.tsx
+++ src/components/Content/stories.tsx
@@ -2,16 +2,16 @@ import { Meta, Story } from "@storybook/react";
 
 import { Content, ContentProps } from ".";
 
-import mockContent from "./mock";
+import { mockcontent } from "./mock";
 
 export default {
   title: "components/Content",
   component: Content,
-  args: mockContent,
+  args: mockcontent,
 } as Meta;
 
 export const Basic: Story<ContentProps> = (args) => (
-  <div style={{ maxWidth: "120rem", margin: "48px auto", padding: "4px" }}>
+  <div style={{ maxWidth: "70rem", margin: "48px auto", padding: "4px" }}>
     <Content {...args} />
   </div>
 );
